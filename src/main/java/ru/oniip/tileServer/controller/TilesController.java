04d79d6f9dcb742d1399d36905737268ad66121b
AVZhykov <avzhykov@gmail.com>
2024-01-15 13:07:14 +0600

    Добавлен метод  для получения тайла @GetMapping

--- src/main/java/ru/oniip/tileServer/controller/TilesController.java
+++ src/main/java/ru/oniip/tileServer/controller/TilesController.java
@@ -3,6 +3,7 @@ package ru.oniip.tileServer.controller;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,10 @@ import java.io.InputStream;
 @RestController
 public class TilesController {
 
-    @GetMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
-    public @ResponseBody byte[] getImage() throws IOException {
-        File file = new File("maps/omsk_osm/10/719/323.png");
+    @GetMapping(value = "/osm", produces = MediaType.IMAGE_PNG_VALUE)
+    @ResponseBody
+    public byte[] getTile(@RequestParam String schema, int z, int x, int y) throws IOException {
+        File file = new File("maps/" + schema + "/" + z + "/" + x + "/" + y +".png");
         InputStream in = new FileInputStream(file);
         return IOUtils.toByteArray(in);
     }
