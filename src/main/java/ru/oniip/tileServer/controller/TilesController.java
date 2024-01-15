225d89c38860fbcfc93f7a6335c2ec4b2e5d9756
AVZhykov <avzhykov@gmail.com>
2024-01-15 13:33:46 +0600

    Добавлено логирование в загрузку тайлов

--- src/main/java/ru/oniip/tileServer/controller/TilesController.java
+++ src/main/java/ru/oniip/tileServer/controller/TilesController.java
@@ -1,6 +1,8 @@
 package ru.oniip.tileServer.controller;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,16 @@ import java.io.InputStream;
 @RestController
 public class TilesController {
 
-    @GetMapping(value = "/osm", produces = MediaType.IMAGE_PNG_VALUE)
+    private static final Logger logger = LoggerFactory.getLogger(TilesController.class);
+
+    @GetMapping(value = "/", produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
     public byte[] getTile(@RequestParam String schema, int z, int x, int y) throws IOException {
-        File file = new File("maps/" + schema + "/" + z + "/" + x + "/" + y +".png");
+        String urlTile = "maps/" + schema + "/" + z + "/" + x + "/" + y +".png";
+        logger.info("Загружается тайл " + urlTile);
+        File file = new File(urlTile);
         InputStream in = new FileInputStream(file);
         return IOUtils.toByteArray(in);
     }
+
 }
